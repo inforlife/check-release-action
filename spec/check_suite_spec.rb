d451899d4541896f319649bd7447a239c9ba666f
Sigfrid Dusci <sigfrid@users.noreply.github.com>
2025-06-04 11:02:17 +0200

    Add specs about feedback messages

--- spec/check_suite_spec.rb
+++ spec/check_suite_spec.rb
@@ -1,5 +1,4 @@
 ENV["GITHUB_REPOSITORY"] = "inforlife/existing-repo"
-ENV["GITHUB_ACCESS_TOKEN"] = "dummy"
 
 require "lib/check_suite"
 
@@ -9,10 +8,20 @@ RSpec.describe CheckSuite do
     allow(STDOUT).to receive(:puts)
   end
 
-  context "when milestone does not exist" do
-    it "exits with status 1" do
+  context "when the milestone does not exist" do
+    before do
       ENV["GITHUB_MILESTONE"] = "2017.0"
+    end
 
+    it "returns a feedback message" do
+      VCR.use_cassette("inexisting_milestone") do
+        allow_any_instance_of(CheckSuite).to receive(:exit)
+
+        expect { CheckSuite.new.run }.to output(/milestone 2017.0 does not exist/i).to_stdout
+      end
+    end
+
+    it "exits with status 1" do
       VCR.use_cassette("inexisting_milestone") do
         expect { CheckSuite.new.run }.to raise_error(SystemExit) do |error|
           expect(error.status).to eq(1)
@@ -21,10 +30,20 @@ RSpec.describe CheckSuite do
     end
   end
 
-  context "when milestone has still at least an open issue" do
-    it "exits with status 1" do
+  context "when the ilestone has still at least an open issue" do
+    before do
       ENV["GITHUB_MILESTONE"] = "2017.2"
+    end
 
+    it "returns a feedback message" do
+      VCR.use_cassette("open_issue") do
+        allow_any_instance_of(CheckSuite).to receive(:exit)
+
+        expect { CheckSuite.new.run }.to output(/at least an issue is still open/i).to_stdout
+      end
+    end
+
+    it "exits with status 1" do
       VCR.use_cassette("open_issue") do
          expect { CheckSuite.new.run }.to raise_error(SystemExit) do |error|
           expect(error.status).to eq(1)
@@ -33,10 +52,20 @@ RSpec.describe CheckSuite do
     end
   end
 
-  context "when not all the issues included in milestone has the expected labels" do
-    it "exits with status 1" do
+  context "when not all the issues included in the milestone has the expected labels" do
+    before do
       ENV["GITHUB_MILESTONE"] = "2018.1"
+    end
 
+    it "returns a feedback message" do
+      VCR.use_cassette("unlabeled_issue") do
+        allow_any_instance_of(CheckSuite).to receive(:exit)
+
+        expect { CheckSuite.new.run }.to output(/at least an issue doesn't have the required labels/i).to_stdout
+      end
+    end
+
+    it "exits with status 1" do
       VCR.use_cassette("unlabeled_issue") do
         expect { CheckSuite.new.run }.to raise_error(SystemExit) do |error|
           expect(error.status).to eq(1)
@@ -46,9 +75,19 @@ RSpec.describe CheckSuite do
   end
 
   context "when tag exists already" do
-    it "exits with status 1" do
+    before do
       ENV["GITHUB_MILESTONE"] = "2017.1"
+    end
 
+    it "returns a feedback message" do
+      VCR.use_cassette("existing_tag") do
+        allow_any_instance_of(CheckSuite).to receive(:exit)
+
+        expect { CheckSuite.new.run }.to output(/tag 2017.1 already exists/i).to_stdout
+      end
+    end
+
+    it "exits with status 1" do
       VCR.use_cassette("existing_tag") do
         expect { CheckSuite.new.run }.to raise_error(SystemExit) do |error|
           expect(error.status).to eq(1)
@@ -58,9 +97,19 @@ RSpec.describe CheckSuite do
   end
 
   context "when at least a required status check did not pass" do
-    it "exits with status 1" do
+    before do
       ENV["GITHUB_MILESTONE"] = "2021.1"
+    end
 
+     it "returns a feedback message" do
+      VCR.use_cassette("failed_check") do
+        allow_any_instance_of(CheckSuite).to receive(:exit)
+
+        expect { CheckSuite.new.run }.to output(/at least a required check didn't pass during the last run on the default branch/i).to_stdout
+      end
+    end
+
+    it "exits with status 1" do
       VCR.use_cassette("failed_check") do
         expect { CheckSuite.new.run }.to raise_error(SystemExit) do |error|
           expect(error.status).to eq(1)
@@ -70,7 +119,17 @@ RSpec.describe CheckSuite do
   end
 
   context "when all the checks pass" do
-    ENV["GITHUB_MILESTONE"] = "2021.1"
+    before do
+      ENV["GITHUB_MILESTONE"] = "2021.1"
+    end
+
+    it "returns a feedback message" do
+      VCR.use_cassette("drafted_release") do
+        allow_any_instance_of(CheckSuite).to receive(:exit)
+
+        expect { CheckSuite.new.run }.to output(/all checks passed. 2021.1 can be released/i).to_stdout
+      end
+    end
 
     it "exits with status 0" do
       VCR.use_cassette("drafted_release") do
